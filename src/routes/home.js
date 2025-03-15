3a61c6c6a4d56af5a198447db313cc7cfcd91a3d
carolacodes <carolacardozog23@gmail.com>
2025-03-14 22:26:03 -0300

    EJS Partials

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,13 +1,20 @@
 const {Router} = require('express')
 const router = Router()
 
+
+router.get('/', (req, res) => {
+    res.render('index', {
+        title: 'index page'
+    })
+})
+
 router.get('/about', (req, res) => {
     const title = "mi pag creada desde express"
-    res.render('index', {title})
+    res.render('about')
 })
 
 router.get('/dashboard', (req, res) => {
-    res.send('dashboard page')
+    res.render('dashboard')
 })
 
 module.exports = router
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,12 +1,8 @@
 const {Router} = require('express')
 const router = Router()
 
-router.get('/profile', (req, res) => {
-    res.send('profile page')
-})
-
-router.get('/UserName', (req, res) => {
-    res.send('Username route')
+router.get('/users', (req, res) => {
+    res.render('users')
 })
 
 module.exports = router
